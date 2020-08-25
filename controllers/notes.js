e5964f30dc69e9eecd35f74ba412eb426033dce7
XTZhu <33222662+XTZhu@users.noreply.github.com>
2020-08-25 18:10:51 +0800

    feat: 添加user

--- controllers/notes.js
+++ controllers/notes.js
@@ -9,47 +9,41 @@ notesRouter.get("/", async (request, response) => {
 
 // get note by id
 notesRouter.get("/:id", async (request, response) => {
-  // try {
   const note = await Note.findById(request.params.id);
   if (note) {
     response.json(note);
   } else {
     response.status(404).end();
   }
-  // } catch (error) {
-  //   next(error);
-  // }
 });
 
 // add note
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
   });
-  // try {
   const savedNote = await note.save();
   response.json(savedNote);
-  // } catch (error) {
-  //   next(error);
-  // }
 });
 
 // delete note by id
 notesRouter.delete("/:id", async (request, response) => {
-  // try {
   await Note.findByIdAndRemove(request.params.id);
   response.status(204).end();
-  // } catch (error) {
-  //   next(error);
-  // }
 });
 
 // update
-notesRouter.put("/:id", (request, response, next) => {
+notesRouter.put("/:id", async (request, response) => {
   const body = request.body;
 
   const note = {
@@ -57,11 +51,10 @@ notesRouter.put("/:id", (request, response, next) => {
     important: body.important,
   };
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
+  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+  });
+  response.json(updatedNote);
 });
 
 module.exports = notesRouter;
--- models/note.js
+++ models/note.js
@@ -13,7 +13,10 @@ const noteSchema = new mongoose.Schema({
   },
   important: {
     type: Boolean,
-    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
   },
 });
 
