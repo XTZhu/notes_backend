d5a9ae709ee4fc9e491396b3c7ad09ec8a1799e4
XTZhu <33222662+XTZhu@users.noreply.github.com>
2020-09-16 23:01:03 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,8 +22,8 @@ module.exports = {
       user: "root",
       host: "47.107.71.183",
       ref: "origin/master",
-      repo: "GIT_REPOSITORY",
-      path: "DESTINATION_PATH",
+      repo: "git@github.com:XTZhu/notes_backend.git",
+      path: "/www/wwwroot/root/note_server/",
       "pre-deploy-local": "",
       "post-deploy":
         "npm install && pm2 reload ecosystem.config.js --env production",
